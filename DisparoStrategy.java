fcc9392c78d60f3cba22e9c2beff50665d40fa03
EliasBravoG <eliasismaelbravo59@gmail.com>
2025-11-21 17:14:28 -0300

    Add files via upload

---
+++ DisparoStrategy.java
@@ -0,0 +1,7 @@
+package puppy.code;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public interface DisparoStrategy {
+    void disparar(Nave4 nave, PantallaJuego juego, SpriteBatch batch);
+}
